4c71e0489f4eb48f8fbd56e0358b24389f9d13cb
Bill Kable <bkable@pivotal.io>
2020-01-08 11:09:15 -0500

    Add tests for MVC lab

---
+++ src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
@@ -0,0 +1,25 @@
+package io.pivotal.pal.tracker;
+
+import java.sql.Time;
+import java.util.List;
+
+public class InMemoryTimeEntryRepository {
+    public TimeEntry create(TimeEntry timeEntry) {
+        return null;
+    }
+
+    public TimeEntry find(Long id) {
+        return null;}
+
+    public List<TimeEntry> list() {
+        return null;
+    }
+
+    public TimeEntry update(Long id, TimeEntry timeEntry) {
+        return null;
+    }
+
+    public void delete(Long id) {
+
+    }
+}
---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntry.java
@@ -0,0 +1,28 @@
+package io.pivotal.pal.tracker;
+
+import java.time.LocalDate;
+
+public class TimeEntry {
+
+    private long id;
+    private long projectId;
+    private long userId;
+    private LocalDate date;
+    private int hours;
+
+    public TimeEntry(long projectId, long userId, LocalDate parse, int i) {
+        
+    }
+
+    public TimeEntry(long timeEntryId, long projectId, long userId, LocalDate parse, int i) {
+        
+    }
+
+    public TimeEntry() {
+
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+}
